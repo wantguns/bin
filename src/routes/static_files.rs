53d96865adbace834460c449a5edaa1906c92a0e
Gunwant Jain <mail@wantguns.dev>
2021-12-26 21:01:56 +0530

    UI: Use a spacier layout
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- src/routes/static_files.rs
+++ src/routes/static_files.rs
@@ -2,7 +2,7 @@ use rocket::fs::NamedFile;
 use rocket::response::status::NotFound;
 use std::path::{Path, PathBuf};
 
-#[get("/<file..>", rank = 3)]
+#[get("/static/<file..>")]
 pub async fn static_files(file: PathBuf) -> Result<NamedFile, NotFound<String>> {
     NamedFile::open(Path::new("static/").join(file))
         .await
