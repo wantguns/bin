5aafe2500a7bb3903d8feb96cfe2ca32bc5c294c
Leonora Tindall <nora@nora.codes>
2022-01-31 20:49:05 -0600

    Redirect on invalid UTF-8 in /p/
    Previously, uploading a paste with invalid UTF-8 and then viewing it
    with the pretty URL would cause a panic.
    With this change, it simply redirects to the raw URL.

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,4 @@
+pub mod maybe_redirect;
 pub mod paste_id;
 pub mod pretty;
 pub mod pretty_syntax;
--- src/models/pretty.rs
+++ src/models/pretty.rs
@@ -10,16 +10,16 @@ static SYNTAXES: &[u8] =
 static THEMES: &[u8] =
     include_bytes!("../../resources/themes/ayu_dark.tmTheme");
 
-pub fn get_pretty_body(path: &Path, ext: &str) -> String {
+pub fn get_pretty_body(path: &Path, ext: &str) -> std::io::Result<String> {
     let ss: SyntaxSet = syntect::dumps::from_binary(SYNTAXES);
 
     let mut theme_cursor = std::io::Cursor::new(THEMES);
     let theme = ThemeSet::load_from_reader(&mut theme_cursor).unwrap();
 
-    let content = fs::read_to_string(path).unwrap();
+    let content = fs::read_to_string(path)?;
     let syntax = ss
         .find_syntax_by_token(ext)
         .unwrap_or_else(|| ss.find_syntax_plain_text());
 
-    highlighted_html_for_string(&content, &ss, syntax, &theme)
+    Ok(highlighted_html_for_string(&content, &ss, syntax, &theme))
 }
